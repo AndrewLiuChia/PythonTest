6b11bb2c265175332c97f520f39326b842c8e40b
DESKTOP-QQBRP66\Andrew <m9721013@gmail.com>
2025-03-29 18:47:46 +0800

    Added number input functionality and fixed Chinese encoding

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+# 存儲輸入的數字
+numbers = []
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    if request.method == 'POST':
+        number = request.form.get('number')
+        if number and number.isdigit():
+            numbers.append(int(number))
+    return render_template('index.html', numbers=numbers)
 
 if __name__ == '__main__':
     app.run(debug=True)
